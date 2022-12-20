0ff9808d28694d597200ab9a96f143e4e88d55da
zhuyuezx <zhuyuezx@gmail.com>
2022-12-20 00:02:36 -0500

    entity original

--- backend/entities.js
+++ backend/entities.js
@@ -1,9 +1,44 @@
-class IUser {
-    constructor() {
-        this.lcid = null;
+export class IUser {
+    // constructor() {
+    //     this.lcid = null;
+    //     this.wxid = null;
+    //     this.firstName = null;
+    //     this.lastName = null;
+    // }
+
+    constructor(userName, userId, homepage, wxid) {
+        this.userName = userName;
+        this.userId = userId;s
+        this.homepage = homepage;
         this.wxid = null;
-        this.firstName = null;
-        this.lastName = null;
     }
 }
 
+export class DailyLog {
+    // date: str
+    // questions: [str]
+    // reviews: [str]
+    constructor(date, questions = [], reviews = []) {
+        this.date = date;
+        this.questions = questions;
+        this.reviews = reviews;
+    }
+}
+
+export class UserHistory {
+    // user: IUser
+    // logs: [DailyLog]
+    constructor(user, logs = []) {
+        this.user = user;
+        this.logs = logs;
+    }
+}
+
+export class UserAllSubmission {
+    // user: IUser
+    // submissions: {str: int}
+    constructor(user, submissions = {}) {
+        this.user = user;
+        this.submissions = submissions;
+    }
+}
