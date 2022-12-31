9045fe5ff8c61f58772f5ff3a4f12ce8f5811a1c
zhuyuezx <zhuyuezx@gmail.com>
2022-12-31 02:36:58 -0500

    resolve page path bug

--- frontend/src/component/utils.js
+++ frontend/src/component/utils.js
@@ -4,18 +4,18 @@ export const problemUrl = "https://leetcode.com/problems/"
 export const userUrl = "https://leetcode.com/"
 
 export const fileClient = axios.create({
-    baseURL: '.',
+    baseURL: './data',
     timeout: 10000
 });
 
 export const getUsers = async () => {
-    return await fileClient.get("/data/leetcoder_ids.json");
+    return await fileClient.get("/leetcoder_ids.json");
 };
 
 export const getDailyStats = async (date) => {
-    return await fileClient.get("/data/daily_stats/" + date + ".json");
+    return await fileClient.get("/daily_stats/" + date + ".json");
 };
 
 export const getUserInfo = async (lcid) => {
-    return await fileClient.get("../data/user_info/" + lcid + ".json");
+    return await fileClient.get("/user_info/" + lcid + ".json");
 }
