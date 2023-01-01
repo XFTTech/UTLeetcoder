d53b9645e501c6820e774f512303e3838dddc29c
zhuyuezx <zhuyuezx@gmail.com>
2023-01-01 17:17:45 -0500

    change date format for number less than 10

--- backend/src/get_daily_logs.js
+++ backend/src/get_daily_logs.js
@@ -4,8 +4,8 @@ const file_path = '../../frontend/public/data/';
 
 const format_date = (date) => {
     const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
+    const month = date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1;
+    const day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
     return `${year}-${month}-${day}`;
 }
 
