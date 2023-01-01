f903eaa2007f878180621209a0af82f7e36c44b0
zhuyuezx <zhuyuezx@gmail.com>
2023-01-01 16:56:18 -0500

    404 page link

--- frontend/src/Pages/Error404.js
+++ frontend/src/Pages/Error404.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Button, Result } from 'antd';
+import { Link } from 'react-router-dom';
 
 const Error404 = () => (
   <Result
     status="404"
     title="404"
     subTitle="Sorry, the page you visited does not exist."
-    extra={<Button type="primary" onClick={() => window.location.href = "/"}>Back Home</Button>}
+    extra={<Link to="/"><Button>Back to Homepage</Button></Link>}
   />
 );
 
