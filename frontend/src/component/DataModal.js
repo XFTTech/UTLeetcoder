48959951d8a2b052b0e7888513de0b755f13701f
zhuyuezx <zhuyuezx@gmail.com>
2023-01-14 00:40:32 -0500

    add ... to end of question to make size conform

--- frontend/src/component/DataModal.js
+++ frontend/src/component/DataModal.js
@@ -11,8 +11,10 @@ const DataModal = (props) => {
     const [did, setDid] = useState(true);
     const [page, setPage] = useState(1);
     const [pageSize, setPageSize] = useState((parseInt)((window.innerHeight * 0.6 - 24 - 32) / 28.28));
+    const [width, setWidth] = useState(window.innerWidth);
     function handleResize() {
         setPageSize((parseInt)((window.innerHeight * 0.6 - 24 - 32) / 28.28));
+        setWidth(window.innerWidth);
     };
     window.addEventListener('resize', handleResize);
 
@@ -31,7 +33,7 @@ const DataModal = (props) => {
     }, [data, difficulty]);
 
     useEffect(() => {
-        if (page * pageSize > data.length) {
+        if ((page - 1) * pageSize > data.length) {
             setPage((parseInt)(data.length / pageSize) + 1);
         }
     }, [pageSize, data, page]);
@@ -40,58 +42,58 @@ const DataModal = (props) => {
         getModalVisible(false);
     };
     return (
-            <Modal
-                open={visible}
-                footer={null}
-                onDestory={handleCancel}
-                onCancel={handleCancel}
-                title={difficulty + ' problems'}
-                width="50%"
-                bodyStyle={{
-                    height: window.innerHeight * 0.6,
-                    overflow: 'auto',
-                    marginTop: '1px',
+        <Modal
+            open={visible}
+            footer={null}
+            onDestory={handleCancel}
+            onCancel={handleCancel}
+            title={difficulty + ' problems'}
+            width="50%"
+            bodyStyle={{
+                height: window.innerHeight * 0.6,
+                overflow: 'auto',
+                marginTop: '1px',
+            }}
+        >
+            {did ? (data.slice((page - 1) * pageSize, Math.min(page * pageSize, data.length)).map((item) => (
+                <Row key={item}>
+                    <Link href={problemUrl + item + '/'}
+                        target="_blank"
+                        onMouseOver={(e) => {
+                            e.target.style.textDecoration = 'underline';
+                        }}
+                        onMouseLeave={(e) => {
+                            e.target.style.textDecoration = 'none';
+                        }}
+                    >
+                        <Typography
+                            style={{
+                                color: color,
+                                fontSize: 18,
+                            }}>
+                            [{item.length > width / 22 ? item.slice(0, (parseInt)(width / 22) - 3) + '...' : item}]
+                        </Typography>
+                    </Link>
+                </ Row>
+            ))) : <Empty />}
+            {did ? <Pagination
+                style={{
+                    // at bottom center
+                    marginTop: '10px',
+                    textAlign: 'center',
+                    position: 'absolute',
+                    bottom: 15,
+                    width: '100%',
                 }}
-            >
-                {did ? (data.slice((page - 1) * pageSize, Math.min(page * pageSize, data.length)).map((item) => (
-                    <Row key={item}>
-                        <Link href={problemUrl + item + '/'}
-                            target="_blank"
-                            onMouseOver={(e) => {
-                                e.target.style.textDecoration = 'underline';
-                            }}
-                            onMouseLeave={(e) => {
-                                e.target.style.textDecoration = 'none';
-                            }}
-                        >
-                            <Typography
-                                style={{
-                                    color: color,
-                                    fontSize: 18,
-                                }}>
-                                [{item}]
-                            </Typography>
-                        </Link>
-                    </ Row>
-                ))) : <Empty />}
-                {did ? <Pagination
-                    style={{
-                        // at bottom center
-                        marginTop: '10px',
-                        textAlign: 'center',
-                        position: 'absolute',
-                        bottom: 15,
-                        width: '100%',
-                    }}
-                    current={page}
-                    total={data.length}
-                    pageSize={pageSize}
-                    showSizeChanger={false}
-                    onChange={(change) => {
-                        setPage(change);
-                    }}
-                /> : null}
-            </Modal>
+                current={page}
+                total={data.length}
+                pageSize={pageSize}
+                showSizeChanger={false}
+                onChange={(change) => {
+                    setPage(change);
+                }}
+            /> : null}
+        </Modal>
     );
 }
 export default DataModal;
